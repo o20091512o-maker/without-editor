ac57e5c6864b6865893d5c78675b7caabcdd9adb
o20091512o-maker <o20091512o@gmail.com>
2026-07-26 17:37:54 +0300

    Remove experimental gl methods from remotion.config.ts for 100% stability

--- backend/remotion/remotion.config.ts
+++ backend/remotion/remotion.config.ts
@@ -2,10 +2,3 @@ import { Config } from "@remotion/cli/config";
 
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
-Config.setGl("swangle");
-Config.setChromiumFlags([
-  "--no-sandbox",
-  "--disable-setuid-sandbox",
-  "--disable-dev-shm-usage",
-  "--disable-gpu"
-]);
